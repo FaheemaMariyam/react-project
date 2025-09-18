5dfc042ba90500db16bc25d91c8a341f62ffa760
Faheema Mariyam <faheemamariyam2102004@gmail.com>
2025-09-18 15:59:42 +0530

    res

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,137 @@
+// import React, { useContext } from "react";
+// import { NavLink } from "react-router-dom";
+// import { FaHeart, FaShoppingCart, FaSignOutAlt, FaUser } from "react-icons/fa";
+// import "./Navbar.css";
+// import { WishlistContext } from "../context/wishlistContext";
+// import { CartContext } from "../context/CartContext";
+// import { AuthContext } from "../context/AuthContext";
+// import LogoutIcon from "../pages/LogoutIcon";
+
+// function Navbar() {
+//   const { wishlist } = useContext(WishlistContext);
+//   const { cart } = useContext(CartContext);
+//   const { login, user } = useContext(AuthContext);
+//   console.log(user);
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light custom-navbar shadow-sm">
+//       <div className="container">
+//         <NavLink
+//           className="navbar-brand fw-bold "
+//           to="/"
+//           // onClick={window.location.reload}
+//         >
+//           ShopEase
+//         </NavLink>
+
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarNav"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+
+//         <div className="collapse navbar-collapse" id="navbarNav">
+//           <ul className="navbar-nav ms-auto me-3">
+//             <li className="nav-item">
+//               <NavLink className="nav-link" to="/">
+//                 Home
+//               </NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link" to="/products">
+//                 Products
+//               </NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link" to="/orders">
+//                 Orders
+//               </NavLink>
+//             </li>
+//           </ul>
+
+//           <ul className="navbar-nav align-items-center">
+//             {/* <li className="nav-item">
+//               <NavLink className="nav-link" to="/wishlist">
+//                 <FaHeart />
+//                 {wishlist.length > 0 && (
+//                   <span className="wishlist-badge">{wishlist.length}</span>
+//                 )}
+//               </NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link" to="/cart">
+//                 <FaShoppingCart />
+//                 {cart.length > 0 && (
+//                   <span className="cart-badge">{cart.length}</span>
+//                 )}
+//               </NavLink>
+//             </li> */}
+//             {/* <ul className="navbar-nav align-items-center"> */}
+//             <li className="nav-item">
+//   <NavLink className="nav-link icon-link" to="/wishlist">
+//     <div className="icon-wrapper">
+//       <FaHeart />
+//       {wishlist.length > 0 && (
+//         <span className="wishlist-badge">{wishlist.length}</span>
+//       )}
+//     </div>
+//   </NavLink>
+// </li>
+
+// <li className="nav-item">
+//   <NavLink className="nav-link icon-link" to="/cart">
+//     <div className="icon-wrapper">
+//       <FaShoppingCart />
+//       {cart.length > 0 && (
+//         <span className="cart-badge">{cart.length}</span>
+//       )}
+//     </div>
+//   </NavLink>
+// </li>
+
+//             {!user ? (
+//               <>
+//                 <li className="nav-item">
+//                   <NavLink className="nav-link" to="/signup">
+//                     SignUp
+//                   </NavLink>
+//                 </li>
+
+//                 <li className="nav-item">
+//                   <NavLink className="nav-link" to="/login">
+//                     Login
+//                   </NavLink>
+//                 </li>
+//               </>
+//             ) : null}
+//             {user ? (
+//               <>
+//                 <li className="nav-item">
+//                   <NavLink className="nav-link" to="/profile">
+//                     <FaUser />
+//                   </NavLink>
+//                 </li>
+               
+//                 <li className="nav-item">
+//   <LogoutIcon className="logout-icon" />
+// </li>
+
+//               </>
+//             ) : null}
+//   {/* </ul> */}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { FaHeart, FaShoppingCart, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaHeart, FaShoppingCart, FaUser } from "react-icons/fa";
 import "./Navbar.css";
 import { WishlistContext } from "../context/wishlistContext";
 import { CartContext } from "../context/CartContext";
@@ -10,19 +141,17 @@ import LogoutIcon from "../pages/LogoutIcon";
 function Navbar() {
   const { wishlist } = useContext(WishlistContext);
   const { cart } = useContext(CartContext);
-  const { login, user } = useContext(AuthContext);
-  console.log(user);
+  const { user } = useContext(AuthContext);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light custom-navbar shadow-sm">
       <div className="container">
-        <NavLink
-          className="navbar-brand fw-bold "
-          to="/"
-          onClick={window.location.reload}
-        >
+        {/* Brand */}
+        <NavLink className="navbar-brand fw-bold" to="/">
           ShopEase
         </NavLink>
 
+        {/* Toggler for mobile */}
         <button
           className="navbar-toggler"
           type="button"
@@ -32,95 +161,66 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
+        {/* Menu Items */}
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto me-3">
+          <ul className="navbar-nav ms-auto">
+            {/* Main links */}
             <li className="nav-item">
-              <NavLink className="nav-link" to="/">
-                Home
-              </NavLink>
+              <NavLink className="nav-link" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/products">
-                Products
-              </NavLink>
+              <NavLink className="nav-link" to="/products">Products</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/orders">
-                Orders
-              </NavLink>
+              <NavLink className="nav-link" to="/orders">Orders</NavLink>
             </li>
-          </ul>
 
-          <ul className="navbar-nav align-items-center">
-            {/* <li className="nav-item">
-              <NavLink className="nav-link" to="/wishlist">
-                <FaHeart />
-                {wishlist.length > 0 && (
-                  <span className="wishlist-badge">{wishlist.length}</span>
-                )}
+            {/* Wishlist */}
+            <li className="nav-item">
+              <NavLink className="nav-link icon-link" to="/wishlist">
+                <div className="icon-wrapper">
+                  <FaHeart />
+                  {wishlist.length > 0 && (
+                    <span className="wishlist-badge">{wishlist.length}</span>
+                  )}
+                </div>
               </NavLink>
             </li>
+
+            {/* Cart */}
             <li className="nav-item">
-              <NavLink className="nav-link" to="/cart">
-                <FaShoppingCart />
-                {cart.length > 0 && (
-                  <span className="cart-badge">{cart.length}</span>
-                )}
+              <NavLink className="nav-link icon-link" to="/cart">
+                <div className="icon-wrapper">
+                  <FaShoppingCart />
+                  {cart.length > 0 && (
+                    <span className="cart-badge">{cart.length}</span>
+                  )}
+                </div>
               </NavLink>
-            </li> */}
-            {/* <ul className="navbar-nav align-items-center"> */}
-            <li className="nav-item">
-  <NavLink className="nav-link icon-link" to="/wishlist">
-    <div className="icon-wrapper">
-      <FaHeart />
-      {wishlist.length > 0 && (
-        <span className="wishlist-badge">{wishlist.length}</span>
-      )}
-    </div>
-  </NavLink>
-</li>
+            </li>
 
-<li className="nav-item">
-  <NavLink className="nav-link icon-link" to="/cart">
-    <div className="icon-wrapper">
-      <FaShoppingCart />
-      {cart.length > 0 && (
-        <span className="cart-badge">{cart.length}</span>
-      )}
-    </div>
-  </NavLink>
-</li>
-
+            {/* Auth links */}
             {!user ? (
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/signup">
-                    SignUp
-                  </NavLink>
+                  <NavLink className="nav-link" to="/signup">SignUp</NavLink>
                 </li>
-
                 <li className="nav-item">
-                  <NavLink className="nav-link" to="/login">
-                    Login
-                  </NavLink>
+                  <NavLink className="nav-link" to="/login">Login</NavLink>
                 </li>
               </>
-            ) : null}
-            {user ? (
+            ) : (
               <>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/profile">
                     <FaUser />
                   </NavLink>
                 </li>
-               
                 <li className="nav-item">
-  <LogoutIcon className="logout-icon" />
-</li>
-
+                  <LogoutIcon className="nav-link logout-icon" />
+                </li>
               </>
-            ) : null}
-  {/* </ul> */}
+            )}
           </ul>
         </div>
       </div>
